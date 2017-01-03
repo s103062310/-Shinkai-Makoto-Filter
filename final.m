1827340e812ef6f0d3b54b02926a8de16df4994d
Cindy <cindy2438@gmail.com>
2017-01-03 23:57:21 +0800

    update

--- final.m
+++ final.m
@@ -4,28 +4,47 @@ clear all
 clc
 
 %% read image
-src1 = imread('input10.jpg');
+src1 = imread('input/input1.jpg');
 src2 = im2double(src1);
 [M, N, C] = size(src1);
 
-%% Step1: median filtering
+%% Step1: median filtering - choose filter size by image size
+if(M*N<2000000) s = 3;
+else s = 5;
+end
 blur = zeros(M, N, C);
-blur(:,:,1) = medfilt2(src2(:,:,1));
-blur(:,:,2) = medfilt2(src2(:,:,2));
-blur(:,:,3) = medfilt2(src2(:,:,3));
+blur(:,:,1) = ordfilt2(src2(:,:,1), 5, ones(s, s));
+blur(:,:,2) = ordfilt2(src2(:,:,2), 5, ones(s, s));
+blur(:,:,3) = ordfilt2(src2(:,:,3), 5, ones(s, s));
 
-%% Step2: sharpening
-H = fspecial('laplacian', 1);
-sharp = zeros(M, N, C);
-sharp(:,:,3) = imfilter(blur(:,:,3), H);
-sharp = blur - sharp*1.5;
+%% Step2: color transform
+target = imread('guide/guide10.jpg');
+target = im2double(target);
+color = cf_reinhard(blur, target);
 
-%% Step3: soft light (hue, lightness, blur)
-soft = rgb2hsv(sharp);
-soft(:,:,2) = soft(:,:,2) - 0.01;
-soft(:,:,3) = soft(:,:,3) + 0.15;
+%% Step3: adjust (hue, lightness, blur)
+soft = rgb2hsv(color);
+soft(:,:,2) = soft(:,:,2) + 0.2;
+soft(:,:,3) = soft(:,:,3) + 0.35;
 soft = hsv2rgb(soft);
-soft = imgaussfilt(soft, 0.1);
+
+%% Setp4: choose light source
+imshow(src1);
+[y, x] = ginput(1);
+x = floor(x);
+y = floor(y);
+close
+
+%% Step5: generate light filter (n)
+r = floor(N/10);
+n = floor(M/100);
+light_f = zeros(M, N);
+light_f = circle(light_f, x, y, r);
+%light_f = line(light_f, x, y, n);
+light_f = imgaussfilt(light_f, r/2);
+
+%% Step6: add light
+light = addLight(soft, light_f, M, N);
 
 %% show
 figure
@@ -35,13 +54,19 @@ figure
 imshow(blur);
 title('Step1: median filtering');
 figure
-imshow(sharp);
-title('Step2: sharpening');
+imshow(target)
+title('guide');
 figure
+imshow(color);
+title('color');
+figure
+
+%%
 imshow(soft);
 title('Step3: soft light');
-
-%% save
-imwrite(blur, 'Step1_median_filtering.jpg');
-imwrite(sharp, 'Step2_sharpening.jpg');
-imwrite(soft, 'Step3_soft_light.jpg');
+figure
+imshow(light_f)
+title('light filter');
+figure
+imshow(light)
+title('light');
